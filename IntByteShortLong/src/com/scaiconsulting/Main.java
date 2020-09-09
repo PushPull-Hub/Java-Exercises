2fe565a5e95e748385fded3d936bf4d7ee521ea6
Dhn-Coding <60256254+Dhn-Coding@users.noreply.github.com>
2020-09-09 18:09:41 +0200

    initial commit

---
+++ IntByteShortLong/src/com/scaiconsulting/Main.java
@@ -0,0 +1,29 @@
+package com.scaiconsulting;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        long maxLong = Long.MAX_VALUE ;
+        long minLong = Long.MIN_VALUE;
+        System.out.println("the maximum value of an long is : " + maxLong);
+        System.out.println("the minimum value of an long is : " + minLong );
+
+        int maxInteger = Integer.MAX_VALUE;
+        int minInteger = Integer.MIN_VALUE;
+        System.out.println("the maximum value of an Integer is : " + maxInteger);
+        System.out.println("the minimum value of an Integer is : " +  minInteger);
+
+        short maxShort = Short.MAX_VALUE ;
+        short minShort = Short.MIN_VALUE ;
+        System.out.println("the maximum value of a short : " + maxShort );
+        System.out.println("the minimum value of a short  : " + minShort );
+
+        byte maxByte = Byte.MAX_VALUE ;
+        byte minByte = Byte.MIN_VALUE ;
+        System.out.println("the maximum value of a byte is : " + maxByte );
+        System.out.println("the minimum value of a byte is : " + minByte );
+
+
+    }
+}
