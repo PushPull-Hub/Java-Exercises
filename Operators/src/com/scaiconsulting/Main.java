2477b2b55a724666aaee880de3624065f095badd
Hamza Salvagno <dahhani.hamza1998@gmail.com>
2020-09-10 12:37:57 +0200

    exercises on Operators

---
+++ Operators/src/com/scaiconsulting/Main.java
@@ -0,0 +1,26 @@
+package com.scaiconsulting;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int result = 1 + 2 ;
+        System.out.println("1 + 2 =" + result);
+        int previousResult = result ;
+        System.out.println("previous result is : " + previousResult);
+        result = result + 1 ;
+        System.out.println( "result after adding 1 is : " + result);
+        System.out.println("previous result after adding 1 to result is : " + previousResult);
+
+        // the remainder
+        result = result % 3 ;
+        System.out.println(result); // 1
+
+        // abbreviation
+        result++ ; // result = result + 1
+        result-- ; // result = result -1
+        result+=2 ; // result = result +2
+        result*=10 ; //  result = result * 10
+
+    }
+}
