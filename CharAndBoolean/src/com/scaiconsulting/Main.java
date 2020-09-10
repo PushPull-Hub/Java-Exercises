162bdf8e1f47c9d78024411b588115b8300e7568
Hamza Salvagno <dahhani.hamza1998@gmail.com>
2020-09-10 15:15:53 +0200

    reformating the code

--- CharAndBoolean/src/com/scaiconsulting/Main.java
+++ CharAndBoolean/src/com/scaiconsulting/Main.java
@@ -4,16 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
 
-	    char myChar = 'D' ;
-	    char myUnicodeChar = '\u0044' ;
-        char CopyRightChar = '\u00A9' ;
+        char myChar = 'D';
+        char myUnicodeChar = '\u0044';
+        char CopyRightChar = '\u00A9';
 
-        System.out.println("myChar is :" + myChar );
+        System.out.println("myChar is :" + myChar);
         System.out.println("myUnicodeCahr is :" + myUnicodeChar);
         System.out.println("the copyright symbol : " + CopyRightChar);
 
-        boolean isJavaACoolLanguage = true ;
-        boolean isUserOverFifty = false ; 
+        boolean isJavaACoolLanguage = true;
+        boolean isUserOverFifty = false;
 
     }
 }
