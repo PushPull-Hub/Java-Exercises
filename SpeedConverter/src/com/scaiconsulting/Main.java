d63b1e2045328641048375256ecf465b16d411b7
Hamza Salvagno <dahhani.hamza1998@gmail.com>
2020-09-14 19:54:15 +0200

    Udemy Exercices Part 1 (sezione 4)

---
+++ SpeedConverter/src/com/scaiconsulting/Main.java
@@ -0,0 +1,12 @@
+package com.scaiconsulting;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(SpeedConverter.toMilesPerHour(10.5));
+        SpeedConverter.printConversion(-2);
+        SpeedConverter.printConversion(20);
+
+    }
+
+}
