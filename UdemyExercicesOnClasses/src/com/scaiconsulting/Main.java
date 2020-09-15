8b0814f54ade6809b7791b6c38868c1a388b92e5
Hamza Salvagno <dahhani.hamza1998@gmail.com>
2020-09-15 16:36:08 +0200

    exercises on Inheritance ( sezione 6 )

--- UdemyExercicesOnClasses/src/com/scaiconsulting/Main.java
+++ UdemyExercicesOnClasses/src/com/scaiconsulting/Main.java
@@ -31,5 +31,13 @@ public class Main {
 
         bobsAccount.deposit(51.0);
         bobsAccount.withdrawal(100.0);
+
+        Animal animal = new Animal("Animal", 1, 1, 5, 5);
+
+        Dog dog = new Dog("Yorkie", 8, 20, 2, 4, 1, 20, "long silky");
+//        dog.eat();
+        dog.walk();
+//        dog.run();
+
     }
 }
