e22aa825c5f8fafeeff1dfbc4dbff964675f958c
Hamza Salvagno <dahhani.hamza1998@gmail.com>
2020-09-22 13:27:51 +0200

    Exercises on Innner & Abstract classes

---
+++ InnerAndAbstractClasses/src/AbstractClassesChallenge/ListItem.java
@@ -0,0 +1,4 @@
+package AbstractClassesChallenge;
+
+public class ListItem {
+}
---
+++ InnerAndAbstractClasses/src/com/scaiconsulting/ISaveable.java
@@ -0,0 +1,9 @@
+package com.scaiconsulting;
+
+import java.util.List;
+
+public interface ISaveable {
+    List<String> write();
+    void read(List<String> savedValues);
+
+}
